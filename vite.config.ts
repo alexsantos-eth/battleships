4431be4694ecfe10d80569faad30b2bf9d8bd3c0
alexsantos-eth <alexdsantosv@gmail.com>
2025-07-25 01:55:39 -0600

    [ Fix ] vite alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,24 @@
+/// <reference types="node" />
 import path from "path";
+import { fileURLToPath } from "url";
 import { defineConfig } from "vite";
-
 import react from "@vitejs/plugin-react";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
-      "@/components": path.resolve(__dirname, "./src/components"),
-      "@/config": path.resolve(__dirname, "./src/config"),
-      "@/hooks": path.resolve(__dirname, "./src/hooks"),
-      "@/stores": path.resolve(__dirname, "./src/stores"),
-      "@/utils": path.resolve(__dirname, "./src/utils"),
-      "@/ui": path.resolve(__dirname, "./src/ui"),
-      "@/env": path.resolve(__dirname, "./src/env"),
+      "@": path.resolve(__dirname, "src"),
+      "@/components": path.resolve(__dirname, "src/components"),
+      "@/config": path.resolve(__dirname, "src/config"),
+      "@/hooks": path.resolve(__dirname, "src/hooks"),
+      "@/stores": path.resolve(__dirname, "src/stores"),
+      "@/utils": path.resolve(__dirname, "src/utils"),
+      "@/ui": path.resolve(__dirname, "src/ui"),
+      "@/env": path.resolve(__dirname, "src/env"),
     },
   },
 });
